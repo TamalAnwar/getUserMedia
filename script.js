e5530aae915f69e64038333ac8d1d1eeb94ee4cf
Tamal Anwar <tamalanwaronline@gmail.com>
2019-04-03 14:31:59 +0600

    Added buttons

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let localVideo = document.querySelector('#localVideo');
 
 let localStream;
 
+let turnOn = document.querySelector('#turnOn');
+let turnOff = document.querySelector('#turnOff');
+
 function gotLocalMediaStream(mediaStream) {
   localStream = mediaStream;
   localVideo.srcObject = mediaStream;
@@ -15,7 +18,16 @@ function handleLocalMediaError(err) {
   console.log('navigator.getUserMedia() error:', err);
 }
 
-navigator.mediaDevices
-  .getUserMedia(mediaStreamConstrains)
-  .then(gotLocalMediaStream)
-  .catch(handleLocalMediaError);
+function startCamera() {
+  navigator.mediaDevices
+    .getUserMedia(mediaStreamConstrains)
+    .then(gotLocalMediaStream)
+    .catch(handleLocalMediaError);
+}
+
+function stopCamera() {
+  localStream.getVideoTracks()[0].stop();
+}
+
+turnOn.addEventListener('click', startCamera);
+turnOff.addEventListener('click', stopCamera);
